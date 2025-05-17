63f7526972daf4bdea08bea402db68723c4049ef
farahAlturk <farahaasemalturk@gmail.com>
2025-05-17 17:18:25 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-let myLead = []
-const inputEl = document.getElementById("input-el")
-const inputButton = document.getElementById("input-btn")
-const tapButton = document.getElementById("tap-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count=0
 
-if (leadsFromLocalStorage) {
-    myLead = leadsFromLocalStorage
-    render(myLead)
+function increment(){
+    count++
+    countEl.textContent = count 
 }
 
-tapButton.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        myLead.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLead))
-        render(myLead)
-    })
-})
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0 
 }
-
-deleteBtn.addEventListener("dblclick", function () {
-    localStorage.clear()
-    myLead = []
-    render(myLead)
-})
-
-inputButton.addEventListener("click", function () {
-    myLead.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLead))
-    render(myLead)
-})
-
